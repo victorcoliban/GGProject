03b7492acd9bef638edbe0c2530eb77461152aee
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2021-11-04 18:43:56 +0200

    add anchor with link

--- script.js
+++
@@ -1,9 +0,0 @@
-const parentContainer = document.querySelector('.read-more-container');
-parentContainer.addEventListener('click', event => {
-    const current = event.target;
-    const isReadMoreBtn = current.className.includes('read-more-btn');
-    if(!isReadMoreBtn) return;
-    const currentText = event.target.parentNode.querySelector('.read-more-text');
-    currentText.classList.toggle('read-more-text--show');
-    current.currentText = current.textContent.includes('Read more') ? "Read less..." : "Read more...";
-})
