4a207e9f46b08967db9e39746cc2e4780e0bd104
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2022-01-25 16:39:49 +0200

    solved first JS exercises

---
+++ JS Exercises/scripts.js
@@ -0,0 +1,146 @@
+// 1. You will be given an array of drinks, with each drink being an object with two properties: name and price.
+
+// Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+// Assume that the following array of drink objects needs to be sorted:
+
+// 1
+
+let drinks = [
+    { name: "lemonade", price: 50 },
+    { name: "lime", price: 10 }
+];
+
+function sortDrinkByPrice1(a, b) {
+    if ( a.price < b.price ){
+        return -1;
+    }
+    if ( a.price > b.price ){
+    return 1;
+    }
+    return 0;
+}
+
+console.log(drinks.sort(sortDrinkByPrice1));
+
+// 2
+
+const sortDrinkByPrice2 = drinks.sort((a, b) => parseInt(a.price) - parseInt(b.price));
+
+console.log(sortDrinkByPrice2);
+
+// 2. A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is,
+
+// knowing the following rules:
+
+// •    The wanted word is in lowercase.
+
+// •    The crowd of letters is all in uppercase.
+
+// •    Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+
+let wordInString = "UcUNFYGaFYFYGtNUH";
+
+const detectWord = () => {
+    var output = '';
+    for (let i = 0; i < wordInString.length; i++) {
+        let character = wordInString.charAt(i);
+        if (character == character.toLowerCase()) {
+            output = output + character;
+        }
+    }
+    console.log(output)
+}
+
+detectWord()
+
+// 3. In this challenge you will be given a relation between two numbers, written as a string. Write a function that determines
+
+// if the relation is true or false.
+
+let str = "8<7";
+
+const isTrue01 = expression => {
+    const operators = {
+        '=': (a, b) => a === b,
+        '<': (a, b) => a < b,
+        '>': (a, b) => a > b
+    },
+        [, a, operator, b] = expression.match(/^(\d+)(\D+)(\d+)$/);
+    return operators[operator]?.(+a, +b) || false;
+}
+
+console.log(isTrue01(str))
+
+const isTrue02 = expression => {
+if (eval(expression) === true) {
+    console.log("true");
+    }
+else {
+    console.log("false");
+    }
+}
+
+isTrue02(str)
+
+// aceeasi functie ca mai sus scrisa altfel
+
+const isTrue03 = expression => {
+    console.log(eval(expression) ? true: (eval(expression) === true));
+}
+
+isTrue03(str)
+
+// 4. Given an object of how many more pages each ink color can print, output the maximum number of pages the printer can print
+
+// before any of the colors run out.
+
+const inkLevels = {
+    "cyan": 23,
+    "magenta": 12,
+    "yellow": 10
+};
+
+let result = () => {
+    const minValue = Math.min.apply(null, Object.values(inkLevels));
+    console.log(minValue)
+}
+
+result()
+
+// 5. A set is a collection of unique items. A set can be formed from an array from removing all duplicate items.
+
+// Create a function that sorts an array and removes all duplicate items from it.
+
+const array = [1, 3, 3, 5, 5, 5];
+
+let set01 = () => new Set(array);
+
+let newArray = Array.from(set01());
+
+console.log(newArray)
+
+// hasOwnProperty()
+// do it with count 
+// reduce()
+
+// const removeDup = array => {
+
+// } 
+
+const removeDup = array => {
+    let prims = {"boolean":{}, "number":{}, "string":{}}, objs = [];
+
+    return array.filter(item => {
+        let type = typeof item;
+        if(type in prims)
+            return prims[type].hasOwnProperty(item) ? false : (prims[type][item] = true);
+        else
+            return objs.indexOf(item) >= 0 ? false : objs.push(item);
+    });
+}
+
+console.log(removeDup(array))
+
+
+
