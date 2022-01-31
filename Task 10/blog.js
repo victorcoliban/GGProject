a2adfc81c57ebbfe2170bd4ae9c798e14c3cf935
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2022-01-31 17:51:28 +0200

    Completed Task 10

--- Task 10/blog.js
+++ Task 10/blog.js
@@ -1,43 +1,81 @@
 // 2
 
-const array = [
-    {id: "1"},
-    {title: "Global Grads"},
-    {description: "Blog"},
-    {date: "26/01/2022"},
-    {image: "dog.png"}
+console.log("****** Ex 2 ******")
+
+let array = [
+    {
+    id: "1",
+    title: "Cocker Spaniel",
+    description: "description of Cocker Spaniel",
+    date: "02/01/2022",
+    image: "cocker.jpg"
+    },
+    {
+    id: "2",
+    title: "Basset Hound",
+    description: "description of Basset Hound",
+    date: "03//01/2022",
+    image: "basset.jpg"
+    },
+    {
+    id: "3",
+    title: "Black Golden Retriever",
+    description: "description of Black Golden Retriever",
+    date: "04/01/2022",
+    image: "retriever.jpeg"
+    },
+    {
+    id: "4",
+    title: "Beagle",
+    description: "description of Beagle",
+    date: "05/01/2022",
+    image: "beagle.jpg"
+    },
+    {
+    id: "5",
+    title: "German Sheperd",
+    description: "description of German Sheperd",
+    date: "06/01/2022",
+    image: "germanSheperd.jpg"
+    },
 ];
 
 console.log(array);
 
-// 3
-// Create a method addPost(){} and here try to add 2 more post object to original array. console.log the array
-// after this change. Should have 7 items now. 
+// // 3
+// // Create a method addPost(){} and here try to add 2 more post object to original array. console.log the array
+// // after this change. Should have 7 items now. 
 
-const addPost = (a) => {
-    array.push(a);
-    console.log(array)
+console.log("****** Ex 3 ******")
+
+const addPost = () => {
+    let newArray = [...array];
+    newArray.push({id: "6", title: "post01", description: "description of post01", date: "31/01/2022", image: "post01.jpg"},
+                  {id: "7", title: "post02", description: "description of post02", date: "01/02/2022", image: "post02.jpg"});
+    console.log(newArray)
 }
 
-addPost({post01: "something"})
-addPost({post02: "something else"})
+addPost()
 
+// // 4. Create a method changePost(){} 
+// // and change the title for second post in the array to "Second blog post". console.log the list after. 
+// // Should have the name changed for second post. 
 
+console.log("****** Ex 4 ******")
 
-// 4. Create a method changePost(){} 
-// and change the title for second post in the array to "Second blog post". console.log the list after. 
-// Should have the name changed for second post. 
-
-const changePost = (index) => {
-    let result = array[index]['title'] = "Second blog post";
-    console.log(result);
+const changePost = () => {
+    let newArray = [...array];
+    newArray[1].title = "Second blog post";
+    console.log(newArray)
 }
 
- changePost(1)
+  changePost()
 
 // 5
 // Create a method selectOddPost(){} and return the odd posts here. For example the posts 
 // with id: 1,3,5. console.log the list after that. 
+
+console.log("****** Ex 5 ******")
 
 const selectOddPost = () => {
     let result = array.filter(function(element, index, array) {
@@ -48,6 +86,23 @@ const selectOddPost = () => {
 
 selectOddPost()
 
-// // 6 
-// Create a method showRecentPost(){} and return here the post 
-// with the most recent date. console.log the post at the end. 
+// // // 6 
+// // Create a method showRecentPost(){} and return here the post 
+// // with the most recent date. console.log the post at the end. 
+
+console.log("****** Ex 6 ******")
+
+// const showRecentPost = () => {
+//     console.log(array.map(function(element) { return element.date; }).sort().reverse()[0]);
+// }
+
+// showRecentPost()
+
+showRecentPost = () => {
+    array.sort(function(a, b) {
+        return Date.parse(a.date) - Date.parse(b.date);
+    });
+    console.log(array[0])
+}
+
+showRecentPost()
