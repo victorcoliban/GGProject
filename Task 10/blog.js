950d952d8f6da573850846524ed02c5d276260fe
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2022-02-01 12:25:43 +0200

    code adjustments

--- Task 10/blog.js
+++ Task 10/blog.js
@@ -2,71 +2,71 @@
 
 console.log("****** Ex 2 ******")
 
-let array = [
+let blogItems = [
     {
-    id: "1",
-    title: "Cocker Spaniel",
-    description: "description of Cocker Spaniel",
-    date: "02/01/2022",
-    image: "cocker.jpg"
+        id: "1",
+        title: "Cocker Spaniel",
+        description: "description of Cocker Spaniel",
+        date: "02/01/2022",
+        image: "cocker.jpg"
     },
     {
-    id: "2",
-    title: "Basset Hound",
-    description: "description of Basset Hound",
-    date: "03//01/2022",
-    image: "basset.jpg"
+        id: "2",
+        title: "Basset Hound",
+        description: "description of Basset Hound",
+        date: "03//01/2022",
+        image: "basset.jpg"
     },
     {
-    id: "3",
-    title: "Black Golden Retriever",
-    description: "description of Black Golden Retriever",
-    date: "04/01/2022",
-    image: "retriever.jpeg"
+        id: "3",
+        title: "Black Golden Retriever",
+        description: "description of Black Golden Retriever",
+        date: "04/01/2022",
+        image: "retriever.jpeg"
     },
     {
-    id: "4",
-    title: "Beagle",
-    description: "description of Beagle",
-    date: "05/01/2022",
-    image: "beagle.jpg"
+        id: "4",
+        title: "Beagle",
+        description: "description of Beagle",
+        date: "05/01/2022",
+        image: "beagle.jpg"
     },
     {
-    id: "5",
-    title: "German Sheperd",
-    description: "description of German Sheperd",
-    date: "06/01/2022",
-    image: "germanSheperd.jpg"
+        id: "5",
+        title: "German Sheperd",
+        description: "description of German Sheperd",
+        date: "06/01/2022",
+        image: "germanSheperd.jpg"
     },
 ];
 
-console.log(array);
+console.log(blogItems);
 
 // // 3
-// // Create a method addPost(){} and here try to add 2 more post object to original array. console.log the array
+// // Create a method addPost(){} and here try to add 2 more post object to original blogItems. console.log the blogItems
 // // after this change. Should have 7 items now. 
 
 console.log("****** Ex 3 ******")
 
 const addPost = () => {
-    let newArray = [...array];
-    newArray.push({id: "6", title: "post01", description: "description of post01", date: "31/01/2022", image: "post01.jpg"},
+    let newblogItems = [...blogItems];
+    newblogItems.push({id: "6", title: "post01", description: "description of post01", date: "31/01/2022", image: "post01.jpg"},
                   {id: "7", title: "post02", description: "description of post02", date: "01/02/2022", image: "post02.jpg"});
-    console.log(newArray)
+    console.log(newblogItems)
 }
 
 addPost()
 
 // // 4. Create a method changePost(){} 
-// // and change the title for second post in the array to "Second blog post". console.log the list after. 
+// // and change the title for second post in the blogItems to "Second blog post". console.log the list after. 
 // // Should have the name changed for second post. 
 
 console.log("****** Ex 4 ******")
 
 const changePost = () => {
-    let newArray = [...array];
-    newArray[1].title = "Second blog post";
-    console.log(newArray)
+    let newblogItems = [...blogItems];
+    newblogItems[1].title = "Second blog post";
+    console.log(newblogItems)
 }
 
   changePost()
@@ -77,13 +77,19 @@ const changePost = () => {
 
 console.log("****** Ex 5 ******")
 
-const selectOddPost = () => {
-    let result = array.filter(function(element, index, array) {
-        return (index % 2 === 0);
-    });
-    console.log(result);
-}
+// const selectOddPost = () => {
+//     let oddItems = blogItems.filter(function(element, index, blogItems) {
+//         return (index % 2 === 0);
+//     });
+//     console.log(oddItems);
+// }
 
+// selectOddPost()
+
+const selectOddPost = () => {
+    let oddItems = blogItems.filter((eleent, index, blogItems) => index % 2 === 0);
+    console.log(oddItems)
+}
 selectOddPost()
 
 // // // 6 
@@ -93,16 +99,16 @@ selectOddPost()
 console.log("****** Ex 6 ******")
 
 // const showRecentPost = () => {
-//     console.log(array.map(function(element) { return element.date; }).sort().reverse()[0]);
+//     console.log(blogItems.map(function(element) { return element.date; }).sort().reverse()[0]);
 // }
 
 // showRecentPost()
 
 showRecentPost = () => {
-    array.sort(function(a, b) {
+    blogItems.sort(function(a, b) {
         return Date.parse(b.date) - Date.parse(a.date);
     });
-    console.log(array[0])
+    console.log(blogItems[0])
 }
 
 showRecentPost()
